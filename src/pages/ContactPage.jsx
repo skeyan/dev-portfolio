7ecc8b9e77ce54a6b54d8ed2d050c83021c12b04
Eva Yan <eyan0749@gmail.com>
2025-11-01 11:43:26 -0400

    Update ContactPage.jsx

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -11,9 +11,9 @@ const Contact = () => {
 
   const contacts = [
     { label: 'Email: (preferred)', value: 'eyan0749@gmail.com', href: 'mailto:eyan0749@gmail.com' },
-    { label: 'LinkedIn:', value: 'sk-evayan', href: 'https://www.linkedin.com/in/sk-evayan/' },
-    { label: 'itch.io:', value: 'masotzhen', href: 'https://masotzhen.itch.io/' },
-    { label: 'Github:', value: 'skeyan', href: 'https://github.com/skeyan' }
+    { label: 'LinkedIn:', value: 'https://www.linkedin.com/in/sk-evayan', href: 'https://www.linkedin.com/in/sk-evayan/' },
+    { label: 'itch.io:', value: 'https://masotzhen.itch.io', href: 'https://masotzhen.itch.io/' },
+    { label: 'Github:', value: 'https://github.com/skeyan', href: 'https://github.com/skeyan' }
   ];
 
   return (
